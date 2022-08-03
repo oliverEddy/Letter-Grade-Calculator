585d91dd1bf95e7bee9de902ee365f5bc1091b17
OLIVER EDDY <oliver.eddy@developersinstitute.co.nz>
2022-08-03 13:56:21 +1200

    getLetterGrade function in untils.js is working and returing the correct letterGrade in the react app, I have began writng the code to refactor the function. Instead of using if statments as the main logic of the function I will use the array methods filter and find. Using the condition statment from my if statments I have began refactoring the code using find and filter, I am fairly sure the logic I have written is correct and will work but I have not implented it yet as i wanted to complete the happy path tests for the function with the logic I knew was working before changing it and then having to write my tests afterwards, Im my mind that would have been alot more difficult and I wanted to have my tests written and passing before refactoing my logic. All happy path tests have been written and are passing with the current verison of the getLetterGrade function, I levergaed the it.each testing feature to reduce repition in my untils.test.js file.

--- client/src/utils.js
+++ client/src/utils.js
@@ -7,6 +7,7 @@ const getLetterGrade = (gradeScale, mark, level) => {
 
     throw new Error("Grade scale not provided.");
   }
+
   if (mark >= 90 && (level = 5)) {
     return "A+";
   }
@@ -81,3 +82,62 @@ const getLetterGrade = (gradeScale, mark, level) => {
 // console.log(level);
 
 module.exports = { getLetterGrade };
+
+/*
+const localGradeScale = [
+  { level: "Level 5", grade: "A+", maxMark: 100, minMark: 90 },
+  { level: "Level 5", grade: "A", maxMark: 89, minMark: 85 },
+  { level: "Level 5", grade: "A-", maxMark: 84, minMark: 80 },
+  { level: "Level 5", grade: "B+", maxMark: 79, minMark: 75 },
+  { level: "Level 5", grade: "B", maxMark: 74, minMark: 70 },
+  { level: "Level 5", grade: "B-", maxMark: 69, minMark: 65 },
+  { level: "Level 5", grade: "C+", maxMark: 64, minMark: 60 },
+  { level: "Level 5", grade: "C", maxMark: 59, minMark: 55 },
+  { level: "Level 5", grade: "C-", maxMark: 54, minMark: 50 },
+  { level: "Level 5", grade: "D", maxMark: 49, minMark: 40 },
+  { level: "Level 5", grade: "E", maxMark: 39, minMark: 0 },
+  { level: "Level 6", grade: "A+", maxMark: 100, minMark: 92 },
+  { level: "Level 6", grade: "A", maxMark: 91, minMark: 85 },
+  { level: "Level 6", grade: "A-", maxMark: 84, minMark: 80 },
+  { level: "Level 6", grade: "B+", maxMark: 79, minMark: 75 },
+  { level: "Level 6", grade: "B", maxMark: 74, minMark: 70 },
+  { level: "Level 6", grade: "B-", maxMark: 69, minMark: 65 },
+  { level: "Level 6", grade: "C+", maxMark: 64, minMark: 60 },
+  { level: "Level 6", grade: "C", maxMark: 59, minMark: 55 },
+  { level: "Level 6", grade: "C-", maxMark: 54, minMark: 50 },
+  { level: "Level 6", grade: "D", maxMark: 49, minMark: 40 },
+  { level: "Level 6", grade: "E", maxMark: 39, minMark: 0 }
+];
+
+ const getLetterGrade = (gradeScale, mark, level) => {
+  if (!Array.isArray(gradeScale) || !gradeScale.length) {
+    throw new Error("Grade scale not provided.");
+  }
+
+  if (level === 5) {
+    const levelFiveGradeScales = gradeScale.filter(
+      (item) => item.level === "Level 5"
+    );
+
+    const found = levelFiveGradeScales.find((item) => {
+      return mark <= item.maxMark && mark >= item.minMark;
+    });
+    return found.grade;
+  }
+
+  if (level === 6) {
+    const levelSixGradeScales = gradeScale.filter(
+      (item) => item.level === "Level 6"
+    );
+
+    const found = levelSixGradeScales.find((item) => {
+      return mark <= item.maxMark && mark >= item.minMark;
+    });
+    return found.grade;
+  }
+};
+
+
+
+ module.exports = { getLetterGrade };
+ */
--- client/src/utils.test.js
+++ client/src/utils.test.js
@@ -147,4 +147,37 @@ describe("getLetterGrade", () => {
       "Grade scale not provided."
     );
   });
+
+  fit.each([
+    ["A+", 90, 5],
+    ["A", 88, 5],
+    ["A-", 82, 5],
+    ["B+", 78, 5],
+    ["B", 72, 5],
+    ["B-", 68, 5],
+    ["C+", 62, 5],
+    ["C", 58, 5],
+    ["C-", 52, 5],
+    ["D", 48, 5],
+    ["E", 39, 5],
+    ["A+", 92, 6],
+    ["A", 88, 6],
+    ["A-", 84, 6],
+    ["B+", 78, 6],
+    ["B", 74, 6],
+    ["B-", 68, 6],
+    ["C+", 64, 6],
+    ["C", 58, 6],
+    ["C-", 54, 6],
+    ["D", 48, 6],
+    ["E", 39, 6],
+  ])(
+    "getLetterGrade function should return %s letter grade based on user inputs, mark %s and level %s",
+    (expectedGrade, inputMark, inputLevel) => {
+      const util = gradeScale;
+
+      const resultGrade = getLetterGrade(util, inputMark, inputLevel);
+      expect(resultGrade).toBe(expectedGrade);
+    }
+  );
 });
