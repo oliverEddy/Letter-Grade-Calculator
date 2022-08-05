0e6146a1e315615035a2fe2d08223501a643f411
OLIVER EDDY <oliver.eddy@developersinstitute.co.nz>
2022-08-05 15:17:13 +1200

    resolving merge conflicts.

--- server/router.js
+++ server/router.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const repository = require("./repository")
+const repository = require("./repository");
 
 router.get("/", async (req, res, next) => {
   try {
-    const result = await repository.getGradeScale()
-    res.json(result)
+    const result = await repository.getGradeScale();
+    res.json(result);
   } catch (err) {
     next(err);
   }
